d20b822678d31d88c4b587a9a40e416edf7d8051
SzymonTokarzProgramista <149623510+SzymonTokarzProgramista@users.noreply.github.com>
2025-03-21 08:21:06 +0100

    xd

---
+++ inzynierka/alphabet.m
@@ -0,0 +1,55 @@
+function letter=alphabet(number)
+    if number==1
+        
+        elseif number==2
+        letter='B';
+        elseif number==3
+        letter='C';
+        elseif number==4
+        letter='D';
+        elseif number==5
+        letter='E';
+        elseif number==6
+        letter='F';
+        elseif number==7
+        letter='G';
+        elseif number==8
+        letter='H';
+        elseif number==9
+        letter='I';
+        elseif number==10
+        letter='J';
+        elseif number==11
+        letter='K';
+        elseif number==12
+        letter='L';
+        elseif number==13
+        letter='M';
+        elseif number==14
+        letter='N';
+        elseif number==15
+        letter='O';
+        elseif number==16
+        letter='P';
+        elseif number==17
+        letter='Q';
+        elseif number==18
+        letter='R';
+        elseif number==19
+        letter='S';
+        elseif number==20
+        letter='T';
+        elseif number==21
+        letter='U';
+        elseif number==22
+        letter='V';
+        elseif number==23
+        letter='W';
+        elseif number==24
+        letter='X';
+        elseif number==25
+        letter='Y';
+        elseif number==26
+        letter='Z';
+    end 
+end
